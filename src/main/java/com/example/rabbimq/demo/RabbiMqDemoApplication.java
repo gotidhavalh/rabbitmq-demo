5bfbff8a312729561cc4cf4d1fab20af6a12b0ac
Dhaval Goti <gotidhavalh@gmail.com>
2023-05-20 11:45:30 +0530

    commit

---
+++ src/main/java/com/example/rabbimq/demo/RabbiMqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbimq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbiMqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbiMqDemoApplication.class, args);
+    }
+
+}
